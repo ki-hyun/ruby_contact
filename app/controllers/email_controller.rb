8b281ec80c43d530e5d775c3df4e302eadb6c4dd
Kihyun <inblue17@gmail.com>
2015-04-30 16:08:39 +0000

    실제 이메일 전송기능 탑재하기(이메일 접속 허가가 안됨)

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
 	end
 	
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+		gmail = Gmail.connect("inblue17@gmail.com","godqhr12!")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
 	end
 end
